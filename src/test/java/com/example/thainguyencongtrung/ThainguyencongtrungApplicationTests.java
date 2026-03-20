4175547de45acbc58d1430be92ceed8e711adf4c
Công Trung <ct34bgithub@gmail.com>
2026-03-20 10:27:19 +0700

    Initial commit

---
+++ src/test/java/com/example/thainguyencongtrung/ThainguyencongtrungApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.thainguyencongtrung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThainguyencongtrungApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
